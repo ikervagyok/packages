4660e67bc8a9269393793f2392437050358814b5
Lengyel Balazs <ikervagyok@gmail.com>
2019-01-27 02:10:30 +0100

    test

--- release.nix
+++ release.nix
@@ -3,5 +3,9 @@
 let
   pkgs = import nixpkgs {};
 in {
-  inherit (pkgs) zathura (wine { wineRelease = "staging"; wineBuild = "wineWow"; }) chromium firefox-beta;
+  with pkgs;
+  zathura
+  (wine { wineRelease = "staging"; wineBuild = "wineWow"; })
+  chromium
+  firefox-beta
 } 
