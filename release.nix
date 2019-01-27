28731e385e908165b8a3644fda069fcab34dfcd9
Lengyel Balazs <ikervagyok@gmail.com>
2019-01-27 03:36:46 +0100

    test

--- release.nix
+++ release.nix
@@ -4,7 +4,7 @@ let
   pkgs = import nixpkgs {};
 in with pkgs; {
   zathura
-  lib.overrideDerivation wine ( attrs: {
+  pkgs.lib.overrideDerivation wine ( attrs: {
     wineRelease = "staging";
     wineBuild = "wineWow";
   })
