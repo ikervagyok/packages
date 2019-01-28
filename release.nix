42b9b397d4a1df27cd4e0b4eed09415ee595c1e3
Lengyel Balázs <balazs.lengyel@tuwien.ac.at>
2019-01-28 12:16:57 +0100

    test

--- release.nix
+++ release.nix
@@ -3,9 +3,12 @@
 let
   pkgs = import nixpkgs {};
   wine = pkgs.wine.override { wineRelease = "staging"; wineBuild = "wineWow"; };
+  wine = pkgs.wine.override { wineRelease = "unstable"; wineBuild = "wineWow"; };
+  libreoffice = pkgs.libreofficeFresh;
 in {
   inherit (pkgs) zathura;
   wine = wine;
+  libreoffice;
 # inherit (pkgs) wine;
   inherit (pkgs) wineStaging;
   inherit (pkgs) hydra;
