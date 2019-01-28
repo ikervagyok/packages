592a531851b596b5198f4039c587d51f37c6d5f1
Lengyel Balázs <balazs.lengyel@tuwien.ac.at>
2019-01-28 12:12:39 +0100

    test

--- release.nix
+++ release.nix
@@ -2,9 +2,11 @@
 
 let
   pkgs = import nixpkgs {};
+  wine = pkgs.wine.override { wineRelease = "staging"; winebuild = "wineWow"; };
 in {
   inherit (pkgs) zathura;
-  inherit (pkgs) wine;
+  wine;
+# inherit (pkgs) wine;
   inherit (pkgs) wineStaging;
   inherit (pkgs) hydra;
   inherit (pkgs.haskellPackages) xmonad;
